4a2b588bcf6e05136f4db10bde4ec80f2d4158e1
slopjong <git@slopjong.de>
2014-09-02 19:30:13 +0200

    Fixed the display time filter

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -7,4 +7,20 @@ angular
             return (input < 10) ? '0'+input : input;
         };
     })
+    .filter('time', function() {
+        return function(input) {
+            var min = Math.floor(input/60);
+            var sec = input - (min * 60);
+
+            if (min < 10) {
+                min = '0' + min;
+            }
+
+            if (sec < 10) {
+                sec = '0' + sec;
+            }
+
+            return min + ':' + sec;
+        };
+    })
 ;
